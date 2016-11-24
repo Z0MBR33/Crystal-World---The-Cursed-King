656035ec14b9b6dc37170a32e9f19e25a769f99d
Lerkus <Sebastian-Maag@gmx.de>
2016-11-24 13:47:07 +0100

    Auto camera done for now, used gamepad input for playermovement

--- Assets/Scripts/Controll.cs
+++ Assets/Scripts/Controll.cs
@@ -7,10 +7,11 @@ public class Controll : MonoBehaviour
     private GameObject camObj;
     private GameObject playerObj;
 
-    private Vector3 oldDistance;
-    private Vector3 lastCamVelocity = new Vector3(0, 0, 0);
-    private Vector3 camVelocity = new Vector3(0, 0, 0);
-    public float desiredDistance = 7;
+    private Vector3 lastHorizontalCamVelocity = new Vector3(0, 0, 0);
+    private Vector3 lastVerticalCamVelocity = new Vector3(0, 0, 0);
+
+    public float desiredHorizontalDistance = 7;
+    public float desiredVerticalDistance = 4;
 
     public int speed = 10;
 
@@ -19,32 +20,63 @@ public class Controll : MonoBehaviour
         mr = ObjectPool.getObjectPool();
         playerObj = mr.getPlayer();
         camObj = mr.getCamera();
+
+        camObj.transform.position = playerObj.transform.position + new Vector3(1, 1, 1);
     }
 
     void FixedUpdate()
     {
+        playerMovement();
+        camHorizontalMovement();
+        camVerticalMovement();
+    }
 
+    void playerMovement()
+    {
         Vector3 playerVelo = new Vector3(0, 0, 0);
-        Vector3 camVelo = new Vector3(0, 0, 0);
-
-        playerVelo += Vector3.Scale(camObj.transform.forward, new Vector3(1, 1, 1)) * Input.GetAxisRaw("Vertical");
-        playerVelo += Vector3.Scale(camObj.transform.right, new Vector3(1, 1, 1)) * Input.GetAxisRaw("Horizontal");
+        playerVelo += Vector3.Scale(camObj.transform.forward, new Vector3(1, 0, 1)) * Input.GetAxisRaw("Vertical");
+        playerVelo += Vector3.Scale(camObj.transform.right, new Vector3(1, 0, 1)) * Input.GetAxisRaw("Horizontal");
         playerVelo.Normalize();
         playerVelo *= speed;
         playerObj.GetComponent<CharacterController>().SimpleMove(playerVelo);
+    }
+    void camHorizontalMovement()
+    {
+        Vector3 camHorizontalVelo = new Vector3(0, 0, 0);
+        Vector3 oldDistance;
 
-        oldDistance = playerObj.transform.position - camObj.transform.position;
-        camVelocity = camObj.transform.forward;
-        camVelocity *= (Mathf.Abs(oldDistance.magnitude) - desiredDistance);
-        camVelocity *= (Mathf.Abs(oldDistance.magnitude) - desiredDistance);
-        //camVelocity *= speed;
-        //camVelo *= 0.1f;
-        if (oldDistance.sqrMagnitude < desiredDistance * desiredDistance)
+        oldDistance = playerObj.transform.position - camObj.transform.position; //Vector cam to player
+        oldDistance = Vector3.Scale(oldDistance, new Vector3(1, 0, 1));
+        camObj.transform.LookAt(camObj.transform.position + oldDistance);
+        camHorizontalVelo = camObj.transform.forward;
+        camHorizontalVelo *= (Mathf.Abs(oldDistance.magnitude) - desiredHorizontalDistance);
+        camHorizontalVelo *= (Mathf.Abs(oldDistance.magnitude) - desiredHorizontalDistance);
+
+        if (oldDistance.sqrMagnitude < desiredHorizontalDistance * desiredHorizontalDistance)
         {
-            camVelocity *= -1;
+            camHorizontalVelo *= -1;
         }
-        camVelocity = (camVelocity + lastCamVelocity)/2;
-        lastCamVelocity = camVelocity;
-        camObj.GetComponent<Rigidbody>().velocity = camVelocity;
+
+        camHorizontalVelo = (camHorizontalVelo + lastHorizontalCamVelocity) / 2;
+        lastHorizontalCamVelocity = camHorizontalVelo;
+        camObj.GetComponent<Rigidbody>().velocity = Vector3.Scale(camHorizontalVelo, new Vector3(1, 0, 1)) + Vector3.Scale(camObj.GetComponent<Rigidbody>().velocity, new Vector3(0, 1, 0));
+    }
+    void camVerticalMovement()
+    {
+        Vector3 camVerticalVelo = new Vector3(0, 0, 0);
+        float oldDistance = (playerObj.transform.position.y + desiredVerticalDistance) - camObj.transform.position.y; // vertical distance, "is" to "should be"
+
+        camVerticalVelo = new Vector3(0, 1, 0);
+        camVerticalVelo *= (Mathf.Abs(oldDistance));
+        camVerticalVelo *= (Mathf.Abs(oldDistance));
+
+        if (oldDistance < 0 )
+        {
+            camVerticalVelo *= -1;
+        }
+
+        camVerticalVelo = (camVerticalVelo + lastVerticalCamVelocity) / 2;
+        lastVerticalCamVelocity = camVerticalVelo;
+        camObj.GetComponent<Rigidbody>().velocity = Vector3.Scale(camVerticalVelo, new Vector3(0, 1, 0)) + Vector3.Scale(camObj.GetComponent<Rigidbody>().velocity, new Vector3(1, 0, 1));
     }
 }
