e8989cba83e26621ff99713cf954d1be46ec4675
Lerkus <Sebastian-Maag@gmx.de>
2016-11-30 16:20:17 +0100

    new portal imported
    but not implemented

--- Assets/Scripts/LevelManager/Isle.cs
+++ Assets/Scripts/LevelManager/Isle.cs
@@ -55,46 +55,4 @@ public class Isle : MonoBehaviour
         }
 
     }
-
-    public void OnMouseDown()
-    {
-
-        GetComponent<Renderer>().material.color = new Color(0, 255, 0);
-
-        IsleAbstract isle = isleAbstract.getIsleUp();
-        if (isle != null)
-        {
-            isle.IsleObj.GetComponent<Renderer>().material.color = new Color(0, 255, 0);
-        }
-
-        isle = isleAbstract.getIsleUpRight();
-        if (isle != null)
-        {
-            isle.IsleObj.GetComponent<Renderer>().material.color = new Color(0, 255, 0);
-        }
-
-        isle = isleAbstract.getIsleDownRight();
-        if (isle != null)
-        {
-            isle.IsleObj.GetComponent<Renderer>().material.color = new Color(0, 255, 0);
-        }
-
-        isle = isleAbstract.getIsleDown();
-        if (isle != null)
-        {
-            isle.IsleObj.GetComponent<Renderer>().material.color = new Color(0, 255, 0);
-        }
-
-        isle = isleAbstract.getIsleDownLeft();
-        if (isle != null)
-        {
-            isle.IsleObj.GetComponent<Renderer>().material.color = new Color(0, 255, 0);
-        }
-
-        isle = isleAbstract.getIsleUpLeft();
-        if (isle != null)
-        {
-            isle.IsleObj.GetComponent<Renderer>().material.color = new Color(0, 255, 0);
-        }
-    }
 }
