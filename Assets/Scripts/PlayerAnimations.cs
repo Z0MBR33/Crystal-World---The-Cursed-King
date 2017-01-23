87a5415060a129e0a567e1dea32343ec92cd77e5
Z0MBR33 <pascal.plewa97@hotmail.de>
2017-01-23 16:34:00 +0100

    Charakter Animationen und Octopus Animationen fix
    Erstellung des PlayerAnimations Script
    Animations status wechseld sich aufgrund der Velocity

---
+++ Assets/Scripts/PlayerAnimations.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerAnimations : MonoBehaviour {
+    private CharacterController charController;
+    private Animator playerAnimator;
+	// Use this for initialization
+	void Start () {
+    charController = GetComponent<CharacterController>();
+    playerAnimator = GetComponent<Animator>();
+	}
+
+    // Update is called once per frame
+    void Update() {
+        if (charController.velocity.sqrMagnitude > 0.01)
+        {
+            Debug.Log("hallo");
+            playerAnimator.SetBool("isWalking", true);
+        }
+        else
+        {
+            playerAnimator.SetBool("isWalking", false);
+        }
+	}
+}
